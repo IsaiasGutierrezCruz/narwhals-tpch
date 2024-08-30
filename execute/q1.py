4f5aab0b20dbba1455e443e845da3a09d6868b71
Isaias Gutierrez Cruz <isaiasgutierrezqfb@gmail.com>
2024-08-30 00:02:59 -0600

    ci: test having an exception

--- execute/q1.py
+++ execute/q1.py
@@ -24,4 +24,5 @@ print(q1.query(IO_FUNCS['pandas[pyarrow]'](lineitem)))
 print(q1.query(IO_FUNCS['polars[lazy]'](lineitem)).collect())
 print(q1.query(IO_FUNCS['pyarrow'](lineitem)))
 print(q1.query(IO_FUNCS['dask'](lineitem)).compute())
+raise ValueError("Stop here")
 
